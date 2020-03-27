0685a5f877ed351aaea708f854c997d98a3d1c52
divyaparmar259 <divyaparmar259@gmail.com>
2020-03-27 14:08:59 +1400

    ConsoleApp done

---
+++ ProductInventoryConsoleApp/Product.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductInventoryConsoleApp
+{
+    public class Product
+    {
+       
+        string productName;
+        float productPrice;
+        
+        Dictionary<string,float> Product1 = new Dictionary<string, float>();
+        public Product (string productName,float productPrice)
+        {
+           this.productName = productName;
+           this.productPrice = productPrice;
+           
+        }
+
+        public void AddProduct()
+        {
+            Console.Clear();
+            Console.WriteLine("How many Products do you want to add");
+            int totalcount = Int32.Parse(Console.ReadLine());
+
+            for(int i =1;i<=totalcount;i++)
+            {               
+                Console.WriteLine("Enter ProductName");
+                productName = Console.ReadLine();
+                Console.WriteLine("Enter ProductPrice");
+                productPrice = Int32.Parse(Console.ReadLine());
+
+
+                Product1.Add(productName, productPrice);
+                Console.WriteLine("Product Successfully Added..");
+                
+            }
+            Console.WriteLine("");
+
+        }
+        public void SearchProduct()
+        {
+            Console.Clear();
+            Console.WriteLine("Do you want to Search Any Product then");
+            Console.WriteLine("Enter ProductName :");
+
+            string oldProduct = Console.ReadLine();
+            if(Product1.ContainsKey(""+oldProduct))
+            {
+                Console.WriteLine("Product Found :");
+                Console.WriteLine(oldProduct);
+
+             }
+            else
+            {
+                Console.WriteLine("Product is not Available");
+            }
+            Console.WriteLine("");
+        }
+
+        public void CountProduct()
+        {
+            Console.Clear();
+            Console.WriteLine("Total Count of Product");
+            Console.WriteLine(Product1.Count);
+            Console.WriteLine("");
+
+        }
+        public void ListAllProduct()
+        {
+            Console.Clear();
+            Console.WriteLine("List of All Products available in Stock");
+            foreach (KeyValuePair< string,float> a in Product1)
+            {
+                Console.WriteLine("ProductName:{0},ProductPrice:{1}", a.Key, a.Value);
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ ProductInventoryConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ProductInventoryConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product product = new Product("proname",1);
+            repeat:
+            Console.WriteLine("Product Inventory System");
+            Console.WriteLine("Choose any One");
+            Console.WriteLine("1. Add Product");
+            Console.WriteLine("2. Search Product");
+            Console.WriteLine("3. Count Product");
+            Console.WriteLine("4. List All the Product");
+            Console.WriteLine("5. Exit");
+
+            int choice = Int32.Parse(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    product.AddProduct();
+                    goto repeat;
+                    
+                case 2:
+                    product.SearchProduct();
+                    goto repeat;
+                case 3 :
+                    product.CountProduct();
+                    goto repeat;
+                case 4:
+                    product.ListAllProduct();
+                    goto repeat;
+                case 5:
+                    break;
+                default:
+                    Console.WriteLine("Choose From the above only");
+                    goto repeat;
+
+            }
+        }
+    }
+}
